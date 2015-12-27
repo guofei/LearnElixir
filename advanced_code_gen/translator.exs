960446ad366117d145c65513a7b7eeec7ff4693f
guofei <guofei1986@gmail.com>
2015-12-27 22:47:23 +0900

    translator

--- advanced_code_gen/translator.exs
+++ advanced_code_gen/translator.exs
@@ -5,11 +5,11 @@ defmodule Translator do
       IO.puts "quote context (#{unquote(__MODULE__)})"
       Module.register_attribute __MODULE__, :locales, accumulate: true, persist: false
       import unquote(__MODULE__), only: [locale: 2]
-      @before_comple unquote(__MODULE__)
+      @before_compile unquote(__MODULE__)
     end
   end
 
-  defmacro __before_comple__(env) do
+  defmacro __before_compile__(env) do
     compile(Module.get_attribute(env.module, :locales))
   end
 
@@ -20,10 +20,70 @@ defmodule Translator do
   end
 
   def compile(translations) do
-    # TBD:
+    translations_ast = for {locale, mappings} <- translations do
+      deftranslations(locale, "", mappings)
+    end
+
+    final_ast = quote do
+      def t(locale, path, bindings \\ [])
+      unquote(translations_ast)
+      def t(_locale, _path, _bindings), do: {:error, :no_translation}
+    end
+
+    IO.puts Macro.to_string(final_ast)
+    final_ast
   end
+
+  defp deftranslations(locale, current_path, mappings) do
+    for {key, val} <- mappings do
+      path = append_path(current_path, key)
+      if Keyword.keyword?(val) do
+        deftranslations(locale, path, val)
+      else
+        quote do
+          def t(unquote(locale), unquote(path), bindings) do
+            unquote(interpolate(val))
+          end
+        end
+      end
+    end
+  end
+
+  defp interpolate(string) do
+    ~r/(?<head>)%\{[^\}]+\}(?<tail>)/
+    |> Regex.split(string, on: [:head, :tail])
+    |> Enum.reduce "", fn
+      <<"%{" <> rest>>, acc ->
+        key = String.to_atom(String.rstrip(rest, ?\}))
+      quote do
+        unquote(acc) <> to_string(Dict.fetch!(bindings, unquote(key)))
+      end
+      segment, acc -> quote do: (unquote(acc) <> unquote(segment))
+    end
+  end
+
+  defp append_path("", next), do: to_string(next)
+  defp append_path(current, next), do: "#{current}.#{next}"
 end
 
-defmodule My do
+defmodule I18n do
   use Translator
+
+  locale "en",
+  flash: [
+    hello: "Hello %{first} %{last}!",
+    bye: "Bye, %{name}!"
+  ],
+  users: [
+    title: "Users",
+  ]
+
+  locale "fr",
+  flash: [
+    hello: "Salut %{first} %{last}!",
+    bye: "Au revoir, %{name}!"
+  ],
+  users: [
+    title: "Utilisateurs",
+  ]
 end
